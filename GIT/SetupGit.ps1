23698879b5e1af5ff4bb52a898797cf2efad1597
APraestegaard <apraestegaard@gmail.com>
2016-09-02 16:54:01 +0200

    Added folder-picker

--- GIT/SetupGit.ps1
+++ GIT/SetupGit.ps1
@@ -1,4 +1,22 @@
-﻿Set-Location -Path 'C:\Git\apraestegaard@GitHub'
+﻿Function Select-FileDirectory
+{
+  param(
+    [string]$Description ='Select Folder',
+    [string]$RootFolder = 'Desktop'
+  )
+
+  $null = [System.Reflection.Assembly]::LoadWithPartialName('System.windows.forms')
+
+  $objForm = New-Object System.Windows.Forms.FolderBrowserDialog
+  $objForm.Rootfolder = $RootFolder
+  $objForm.Description = $Description
+
+  if ($objForm.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK)  {
+    $objForm.SelectedPath
+  }
+}
+
+Set-Location -Path (Select-FileDirectory)
 
 $gitIgnore = New-Item -Name .gitignore
 
